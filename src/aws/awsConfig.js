42dc062b5aefd1016f8ec2c9555b64f986d38dfe
abhinav822 <pabhinav400@gmail.com>
2022-12-06 22:06:21 +0530

    aws-implemented

--- src/aws/awsConfig.js
+++ src/aws/awsConfig.js
@@ -1,24 +1,21 @@
 const AWS = require('aws-sdk')
 
-AWS.config.update(
-    {
-        accessKeyId: "AKIATZ326YKBGWEDLPEA",
-        secretAccessKey: "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",
-        region: "ap-south-1" // if i want to acces
-    }
-)
+AWS.config.update({
+    accessKeyId: "AKIAY3L35MCRZNIRGT6N",
+    secretAccessKey: "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",
+    region: "ap-south-1"
+})
 
 let uploadFile = async (file) => {  //file is the file object
     return new Promise(function (resolve, reject) {  //this line can be wr
         let s3 = new AWS.S3({ apiVersion: '2006-03-01' });  // this line means we are using s3 service
-
-        var uploadParams = {
-            ACL: "private",  
-            Bucket: "swabuckets",
-            Key:  file.originalname,
+        
+        var uploadParams= {
+            ACL: "public-read",
+            Bucket: "classroom-training-bucket",  //HERE
+            Key: "abc/" + file.originalname, //HERE 
             Body: file.buffer
-        }  
-
+        }
 
         s3.upload(uploadParams, function (err, data) {  // this function is used to upload the file to s3 bucket
             if (err) {
@@ -41,7 +38,7 @@ exports.awsrequest = async (req, res) => {
         if(files && files.length>0){
             //upload to s3 and get the uploaded link
             // res.send the link back to frontend/postman
-            let uploadedFileURL= await uploadFile( files[0] )
+            let uploadedFileURL= await uploadFile( files[0] )  // this line means we are calling the function uploadFile and passing the file object 
             res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
         }
         else{
